2e3d8f3931b82e9c7000c7cdb9b7103945c90bcb
Your Name <leobarrientos02@gmail.com>
2022-03-06 21:00:43 -0500

    Front End

---
+++ src/styles.js
@@ -0,0 +1,24 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles((theme) => ({
+  appBar: {
+    borderRadius: 15,
+    margin: "30px 0",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  heading: {
+    color: "#a0281d",
+    textShadow: "4px 3px 0px #fff, 9px 8px 0px rgba(0,0,0,0.15)",
+  },
+  image: {
+    marginLeft: "15px",
+  },
+  [theme.breakpoints.down("sm")]: {
+    mainContainer: {
+      flexDirection: "column-reverse",
+    },
+  },
+}));
